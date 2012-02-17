f0c2fa975aaf7461c119d03c91151efaca2aada1
Vytautas Gimbutas <vytautas@gimbutas.net>
2012-02-17 09:40:30 +0200

    added install file

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+chmod +x osx && ./osx
+rm -rf ~/.vim
+rm -rf ~/.vimrc 
+rm -rf ~/.zshrc 
+rm -rf ~/.gitconfig 
+rm -rf ~/.hgrc
+
+ln -s "$DIR/.vim" ~/.vim
+ln -s "$DIR/.vimrc" ~/.vimrc
+ln -s "$DIR/.zshrc" ~/.zshrc
+ln -s "$DIR/.gitconfig" ~/.gitconfig
+ln -s "$DIR/.hgrc" ~/.hgrc
