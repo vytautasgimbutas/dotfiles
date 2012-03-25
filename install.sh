cbbeb69d2e31f1d4553201403588a12a96b0cea7
Vytautas Gimbutas <vytautas@gimbutas.net>
2012-03-25 13:43:56 +0300

    install file update

--- install.sh
+++ install.sh
@@ -6,11 +6,13 @@ sh $DIR/osx
 rm -rf ~/.vim
 rm -rf ~/.vimrc 
 rm -rf ~/.zshrc 
+rm r-f ~/.bashrc
 rm -rf ~/.gitconfig 
 rm -rf ~/.hgrc
 
 ln -s "$DIR/.vim" ~/.vim
 ln -s "$DIR/.vimrc" ~/.vimrc
 ln -s "$DIR/.zshrc" ~/.zshrc
+ln -s "$DIR/.bashrc" ~/.bashrc
 ln -s "$DIR/.gitconfig" ~/.gitconfig
 ln -s "$DIR/.hgrc" ~/.hgrc
