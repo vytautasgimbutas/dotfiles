cf9483a2eeadc6bc40b98f11baeb9b80fcfd0d23
Vytautas Gimbutas <vytautas@gimbutas.net>
2012-04-13 20:22:37 +0300

    installer

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# ~/bin
+mkdir ~/bin
+
+# run Mac OS X custom configuration
 sh $DIR/osx
 
+# link dotfiles
 for file in {.vim,.vimrc,.custom,.gitattributes,.gitconfig,.gitignore,.hgrc,.inputrc,.pythonrc,.zshrc}; do
 	rm -rf ~/$file && ln -s $DIR/$file ~/$file
 done
