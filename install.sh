2f8fc82bf8aae9154ec9ee47d5c43583310a9a0e
Vytautas Gimbutas <vytautas@gimbutas.net>
2012-07-15 11:00:06 +0300

    php upgrade

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "Running Mac OS X custom configuration..."
 sh $DIR/osx
 
 # Link files
-files="bin .vim .vimrc .custom .giattributes .gitconfig .gitignore .hgrc .inputrc .pythonrc .zshrc"
+files="bin .vim .vimrc .custom .gitattributes .gitconfig .gitignore .hgrc .inputrc .pythonrc .zshrc"
 echo "\nLinking files..."
 for file in $files; do
     echo "  Linking $file to ~"
@@ -16,8 +16,7 @@ done
 
 # nginx, php-fpm, php
 echo "\nInstalling nginx, php-fpm, php configurations"
-rm -rf /usr/local/etc/nginx/nginx.conf /usr/local/etc/nginx/vhosts /usr/local/etc/php-fpm.conf /usr/local/etc/php.ini
+rm -rf /usr/local/etc/nginx/nginx.conf /usr/local/etc/php-fpm.conf /usr/local/etc/php/5.4/php.ini
 ln -s $DIR/config/nginx/nginx.conf /usr/local/etc/nginx/
-ln -s $DIR/config/nginx/vhosts /usr/local/etc/nginx/
 ln -s $DIR/config/php-fpm.conf /usr/local/etc/
-ln -s $DIR/config/php.ini /usr/local/etc/
+ln -s $DIR/config/php.ini /usr/local/etc/php/5.4/
