e897b0b11544249daf9135821d20e01cd5557e01
Vytautas Gimbutas <vytautas@gimbutas.net>
2013-02-17 22:52:37 +0200

    zprezto

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Run Mac OS X custom configuration
@@ -6,10 +6,15 @@ echo "Running Mac OS X custom configuration..."
 sh $DIR/osx
 
 # Link files
-files="bin .zpreztorc .zprezto .tmux.conf .vim .vimrc .custom .gitattributes .gitconfig .gitignore .hgrc .inputrc .pythonrc .zshrc"
+files="bin .zprezto .tmux.conf .vim .vimrc .custom .gitattributes .gitconfig .gitignore .hgrc .inputrc .pythonrc .zshrc"
 echo "\nLinking files..."
 for file in $files; do
     echo "  Linking $file to ~"
     rm -rf ~/$file
     ln -s $DIR/$file ~/$file
+done
+
+# zprezto
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
